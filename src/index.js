9d5bc01dc553fa7b2028b178eab45da4bfeb96a3
Adisinx <gurnoorbhinders@gmail.com>
2024-09-17 01:38:33 +0530

    DBconnect updated

---
+++ src/index.js
@@ -0,0 +1,60 @@
+//require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+// import mongoose from "mongoose"
+//import { DB_NAME } from "./db/constants.js"
+import connectDB from "./db/index.js"
+import { app } from "./app.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+const porti=process.env.PORT||8000;
+
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log('CUSTOM error before dbConnect ',error);
+        throw error;
+    })
+    app.listen(porti,()=>{
+        console.log(`server is running at port:${porti}`)
+    })
+})
+.catch((err)=>{
+    console.error("CUSTOM MONGOOSE CONNECTION ERROR:",err);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+(async()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error",(error)=>{
+             console.log('application not able to talk to db:',error);
+             throw error;
+
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`app is listening on port:${process.env.PORT}`)
+        })
+
+    }
+    catch(error){
+        console.error('error found:',error);
+        throw error;
+    }
+
+})()
+    */
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHandler=(requestHandler)=>{
+    return(req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((error)=>next(error))
+    }
+}
+
+
+// const asyncHandler1=()=>{}
+// const asyncHandler2=(fn)=>{()=>{}}
+// const asyncHandler3=(func)=>async()=>{}
+
+
+// const asyncHandler=()=>async(req,res,next)=>{
+//     try{
+//         await func(req,res,next);
+//     }
+//     catch(error)
+//     {
+//         req.status(err.code||500).json({
+//             success:false,
+//             message:err.message,
+//         })
+//     }
+// }
